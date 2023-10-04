050876bb5954fc09e7203a0ccc4aa87e288ae522
nashdev255 <yutori1107@gmail.com>
2023-10-04 20:06:13 +0900

    add reviewTheSolutions.cs

---
+++ WriteyourfirstC#code/reviewTheSolution.cs
@@ -0,0 +1,7 @@
+// Console.WriteLine("This is the first line.");
+// Console.WriteLine("This is the second line.");
+
+Console.WriteLine("This is the first line.");
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
