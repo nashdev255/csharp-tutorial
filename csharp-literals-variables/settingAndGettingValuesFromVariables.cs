cf3d5a8e44f36166086463d3801fae63e1b7248a
nashdev255 <yutori1107@gmail.com>
2023-10-05 21:20:53 +0900

    add settingAndGettingValuesFromVariables.cs

---
+++ csharp-literals-variables/settingAndGettingValuesFromVariables.cs
@@ -0,0 +1,8 @@
+string firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
