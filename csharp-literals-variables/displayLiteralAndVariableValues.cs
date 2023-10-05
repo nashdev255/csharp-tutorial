644d921b987d7677a0986e2f71d0d016f6f85669
nashdev255 <yutori1107@gmail.com>
2023-10-05 21:43:19 +0900

    add displayLiteralAndVariableValues

---
+++ csharp-literals-variables/displayLiteralAndVariableValues.cs
@@ -0,0 +1,6 @@
+string name = "Bob";
+int numberOfMessages = 3;
+float temperature = 34.4F;
+Console.Write("Hello, " + name + "! ");
+Console.Write("You have " + numberOfMessages + " messages in your inbox. ");
+Console.Write("The temperature is " + temperature + " celsius.");
