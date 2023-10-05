ea90810def2567788bc1a1486dc9f67812173324
nashdev255 <yutori1107@gmail.com>
2023-10-05 21:03:18 +0900

    add literalValues.cs

---
+++ csharp-literals-variables/literalValues.cs
@@ -0,0 +1,8 @@
+Console.WriteLine('b'); // char
+Console.WriteLine(123); // int
+Console.WriteLine(0.25F); // float
+Console.WriteLine(2.625); // double
+Console.WriteLine(12.39816m); // decimal
+// bool
+Console.WriteLine(true);
+Console.WriteLine(false);
